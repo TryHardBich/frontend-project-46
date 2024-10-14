954611883868f838291bec0d1fb2264574a74f45
TryHardBich <fedorgodovikov@gmail.com>
2024-10-14 15:24:31 +0300

    task 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ const program = new Command();
 program
     .description('Compares two configuration files and shows a difference.')
     .version('-V, version')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]  output format')
 ;
 program.parse()
